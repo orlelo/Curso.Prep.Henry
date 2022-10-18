ee4b8a36c18a7083894e1856010f23d541d2ddd3
orlelo <oriana.lemusl@gmail.com>
2022-10-18 01:16:20 -0400

    HOMEWORK 4 COMPLETA

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,14 +6,23 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var gato = {
+    nombre: nombre,
+    edad: edad,
+    meow: function() {
+      return "Meow!";
+    }
+  }
+  return gato;
 }
-
 
 function agregarPropiedad(objeto, property) {
   // Agrega una propiedad al objeto (argumento "objeto") con el valor `null`
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto[property] = null;
+  return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -21,42 +30,72 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+  return objetoMisterioso.numeroMisterioso * 5;
 }
+/* tambien se puede resolver de esta otra manera, pero es mas larga
+var multi = objetoMisterioso.numeroMisterioso * 5;
+return multi; */
 
 function eliminarPropiedad(objeto, unaPropiedad) {
   // Elimina la propiedad de objeto cuyo nombre está pasado por el parametro unaPropiedad 
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+  delete objeto[unaPropiedad]; //esto es si nos piden el codigo mas resumido posible
+  return objeto;
 }
+/* tambien se puede resolver de esta otra manejarla
+es muy buena porque con el condicional se revisa si la propiedad unaPropiedad
+existe o no, lo que podria arrojar error si estuviera undefined
+
+if(objetoMisterioso[unaPropiedad] === undefined){
+  return objeto;
+}
+else{
+  delete objeto[unaPropiedad];
+  return objeto;
+} */
 
 function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+  var usuario = {
+    nombre: nombre,
+    email: email,
+    password: password
+  }
+  return usuario;
 }
 
 function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  if(usuario["email"]) return true;
+  else return false;
 }
-
-
+/* la propiedad "email" va con comillas porque es el nombre definido para esa propiedad
+pero en el caso de abajo, va sin comillas porque como esta como parametro en la funcion
+"tienePropiedad" entonces es un string que puede ser cualquier cosa
+ej. revisar los tests para ver que se usa x y y como nombre de ese parametro */
 
 function tienePropiedad(objeto, propiedad) {
   // Devuelve "true" si el objeto (parámetro "objeto") tiene una propiedad (key) cuyo nombre es igual al valor del argumento "propiedad"
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(objeto[propiedad]){
+    return true;
+  }
+  else return false;
 }
 
 function verificarPassword(usuario, password) {
@@ -64,12 +103,20 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+  return usuario["password"] === password;
 }
+/* if(usuario.password === password) return true;
+else return false;
+} */
+//la igualdad da true en caso de ser verdad, si no, da false
+//devuelve el valor booleano
 
 function actualizarPassword(usuario, nuevaPassword) {
-  // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
+  // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevaPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password = nuevaPassword;
+  return usuario;
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
@@ -77,7 +124,18 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
+  usuario.amigos.push(nuevoAmigo);
+  return usuario;
 }
+/* NOTAS IMPORTANTES
+para bracket notation, se usan comillas porque con esta notación se pueden pasar propiedades que
+no tengan ese nombre especifico, como los parametros (realmente no son el nombre de la propiedad,
+  porque podria ser cualquier nombre).
+  si se hubiera usado sin comillas, seria un parametro (o la variable), no el nombre exacto
+  con las comillas, se hace referencia al nombre especifico de la propiedad, es un nombre definido
+*/
+/* con el dot notation, es como usar las comillas, hace referencia al nombre especifico
+ */
 
 function pasarUsuarioAPremium(usuarios) {
   // "usuarios" es un array de objetos "usuario"
@@ -85,7 +143,17 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  for (var i = 0; i < usuarios.length; i++){
+    usuarios[i].esPremium = true;
+  }
+  return usuarios;
 }
+
+/* usuarios = [{diferentesUsuarios},{},{},{}] 
+diferentesUsuarios = [{
+  esPremium: true;
+}]
+*/
 
 function sumarLikesDeUsuario(usuario) {
   // "usuario" tiene una propiedad llamada "posts" que es un array
@@ -94,7 +162,17 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var sumaLikes = 0;
+  for (var i = 0; i < usuario.posts.length; i++){ //se recorre el array dentro del objeto usuario, no todo lo que hay dentro del usuario
+    sumaLikes = sumaLikes + usuario.posts[i].likes; //o sumaLikes += usuario.posts[i].likes;
+  }
+  return sumaLikes;
 }
+/* usuario.posts = [{post: likes = numero},{},{},{}] 
+post = [{
+  likes: numero entero;
+}]
+*/
 
 function agregarMetodoCalculoDescuento(producto) {
   // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
@@ -106,8 +184,18 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto.calcularPrecioDescuento = function() {
+    return this.precio - (this.precio * this.porcentajeDeDescuento); /* esto es lo mismo que: return producto.precio - (producto.precio * producto.porcentajeDeDescuento); */
+  }
+  return producto;
 }
+/* IMPORTANTE
+el this hace referencia al contexto (de ejecucion) mas cercano,
+en este caso hace referencia al contexto de la funcion.
+se esta usando dentro de un metodo (calcularPrecioDesuento, el cual
+  se esta usando sobre el objeto) de un objeto, entonces el this 
+  esta haciendo referencia a las propiedades de ese objeto.
+*/
 
 // No modificar nada debajo de esta línea
 // --------------------------------
